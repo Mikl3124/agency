cb37b66de086d10fff15a81bf0c3abd7d3c94b6c
Mikl3124 <mickael.delpech@gmail.com>
2022-12-02 22:00:31 +0100

    fix bug links

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -42,8 +42,7 @@ class FrontEndController extends Controller
 
         $category = Category::where('slug', $slug)->first();
         if($category){
-
-          $title = "Le blog de l'actualité digitale - Lyneo";
+          $title = "$category->name | Le blog de l'actualité digitale | Lyneo";
           $meta_description = "Venez suivre toute l'actualité digitale sur le blog de notre agence. Toutes les informations sur l’univers du SEO, des réseaux sociaux, WordPress et de la création de sites web.";
 
             $posts = Post::where('category_id', $category->id)->paginate(9);
